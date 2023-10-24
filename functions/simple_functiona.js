d9fd2d23fcb8df49d8742950c5235b8fa87420c2
Kyleemochoa01 <146892712+Kyleemochoa01@users.noreply.github.com>
2023-10-24 13:43:35 -0700

    finished excercise

---
+++ functions/simple_functiona.js
@@ -0,0 +1,14 @@
+
+
+function listFavoriteThings() {
+    console.log ("Kylee Ochoa")
+    console.log ("Goodfellas")
+    console.log("junior h ")
+}
+
+function listWhyImHere (){
+    console.log("I joined this program to be able to retire my parents one day!")
+}
+
+listFavoriteThings();
+listWhyImHere();
